8dfb3ece21383dc7c702b10da4250bebe9974038
yashwant-mali <yashwantmali555@gmail.com>
2024-10-14 11:20:17 +0530

    addid other alternte for axios

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,26 @@ const app = express();
 app.use(cors()); // Allow requests from frontend
 
 // API endpoint to fetch GitHub users
+// app.get('/api/users', async (req, res) => {
+//   try {
+//     const response = await axios.get('https://api.github.com/users');//git the data present in this api to localhost:5000/api/users
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ message: 'Error fetching users' });
+//   }
+// });
+
+
 app.get('/api/users', async (req, res) => {
   try {
-    const response = await axios.get('https://api.github.com/users');//git the data present in this api to localhost:5000/api/users
-    res.json(response.data);
+    const response = await fetch('https://api.github.com/users');
+    const data = await response.json();
+    res.json(data);
   } catch (error) {
     res.status(500).json({ message: 'Error fetching users' });
   }
 });
+
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
